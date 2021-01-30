eaac7c9130721af25c94cfcfdff515b0f6141f6d
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2021-01-29 19:16:26 -0500

    Render an empty Board component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('renders a board', () => {
+    render(<App />);
+    const board = screen.getByTestId('board');
+    expect(board).toBeInTheDocument();
+  });
+})
---
+++ src/Board.js
@@ -0,0 +1,8 @@
+function Board() {
+  return (
+    <div className="board" data-testid="board">
+    </div>
+  );
+}
+
+export default Board;
