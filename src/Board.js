5880c8ae116af2bfc506e6fac2f9ed18caf14a3f
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2021-01-29 20:33:39 -0500

    Refactor Board

--- src/Board.js
+++ src/Board.js
@@ -2,27 +2,39 @@ import './Board.css';
 import React, { useState } from 'react';
 
 function Board() {
-  const [state, setState] = useState(Array(19).fill(
-    Array(19).fill(null)
-  ))
+  const size = 19;
+  const initialState = [...new Array(size)].map(
+    () => [...new Array(size)].map(() => null)
+  );
+  const [state, setState] = useState(initialState);
+
+  const setCellState = (rowIdx, colIdx, value) => {
+    const newState = [...state];
+    newState[rowIdx] = [...state[colIdx]];
+    newState[rowIdx][colIdx] = value;
+
+    setState(newState);
+  }
 
   return (
     <div className="board" data-testid="board">
       <table>
         <tbody>
-          {state.map((rowState, rowIdx) => <tr key={rowIdx}>
-            {rowState.map((cellState, colIdx) => <td key={colIdx} onClick={() => {
-              const newState = [...state]
-              newState[rowIdx] = [...state[colIdx]]
-              newState[rowIdx][colIdx] = 'black-stone'
-
-              setState(newState)
-            }} className={cellState}>.</td>)}
-          </tr>)}
+          {state.map((rowState, rowIdx) =>
+            <tr key={rowIdx}>
+              {rowState.map((cellState, colIdx) =>
+                <td
+                  key={colIdx}
+                  onClick={() => setCellState(rowIdx, colIdx, 'black-stone')}
+                  className={cellState}
+                >.</td>
+              )}
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
   );
-}
+};
 
 export default Board;
