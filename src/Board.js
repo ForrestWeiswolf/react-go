df01aa55fd79153746c94647ffc06be3871dd995
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2021-01-29 20:13:09 -0500

    Set first cell clicked to black

--- src/Board.js
+++ src/Board.js
@@ -1,19 +1,23 @@
 import './Board.css';
+import React, { useState } from 'react';
 
 function Board() {
-  const state = Array(19).fill(
+  const [state, setState] = useState(Array(19).fill(
     Array(19).fill(null)
-  )
+  ))
 
   return (
     <div className="board" data-testid="board">
-      {/* {state.map((intersectionState, idx) => <div role="row" key={idx}></div>)} */}
       <table>
         <tbody>
-          {state.map((rowState, idx) => <tr key={idx}>
-            {rowState.map((intersection, idx) => <td key={idx}>
-              <span>.</span>
-            </td>)}
+          {state.map((rowState, rowIdx) => <tr key={rowIdx}>
+            {rowState.map((cellState, colIdx) => <td key={colIdx} onClick={() => {
+              const newState = [...state]
+              newState[rowIdx] = [...state[colIdx]]
+              newState[rowIdx][colIdx] = 'black-stone'
+
+              setState(newState)
+            }} className={cellState}>.</td>)}
           </tr>)}
         </tbody>
       </table>
--- src/Board.test.js
+++ src/Board.test.js
@@ -1,4 +1,4 @@
-import { render, screen, within } from '@testing-library/react';
+import { render, screen, within, fireEvent } from '@testing-library/react';
 import Board from './Board';
 
 describe('Board', () => {
@@ -8,8 +8,17 @@ describe('Board', () => {
     expect(rows.length).toBe(19);
 
     rows.forEach(row => {
-      const cells = within(row).queryAllByRole('cell')
-      expect(cells.length).toBe(19)
+      const cells = within(row).queryAllByRole('cell');
+      expect(cells.length).toBe(19);
     })
   });
+
+  it('When a cell is first clicked, it is filled in black', () => {
+    render(<Board />);
+    const cells = screen.queryAllByRole('cell');
+
+    fireEvent.click(cells[12]);
+
+    expect(cells[12]).toHaveClass('black-stone')
+  })
 })
